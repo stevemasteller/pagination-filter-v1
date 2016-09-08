0cef0a8112143f2ac4e09f9916ec35ed38861f72
stevemasteller <stevermasteller@gmail.com>
2016-09-08 09:58:05 -0400

    Added links
    Added links to bottom of the page. Decided not to use jQuery removed
    from index.html.

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// count students
+var studentElements = document.getElementsByClassName("student-item");
+var page = document.getElementsByClassName("page");
+var studentList = document.getElementsByClassName("student-list");
+const STUDENTS_PER_PAGE = 10;
+
+// add links based on student count
+
+
+var createNewLink = function() {
+	document.body.appendChild(link);
+}
+
+var addLinks = function() {
+	var numberOfLinks = Math.ceil( studentElements.length / STUDENTS_PER_PAGE);
+	
+	var div = document.createElement("div");
+	var ul = document.createElement("ul");
+	var li;
+	var a;
+	
+	div.className = "pagination";
+	div.appendChild(ul);
+	
+	for (i = 0; i < numberOfLinks; i++) {
+		li = document.createElement("li");
+	    a = document.createElement("a");
+		
+		a.href = "#";
+		a.innerText = i;
+		
+		li.appendChild(a);
+		ul.appendChild(li);
+	}
+	
+	page[0].appendChild(div);
+}
+
+
+// on load
+addLinks();
